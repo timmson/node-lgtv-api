e05124ed34f266893be02663d939a04d3dd5ab4c
timmson <timmson666@mail.ru>
2021-01-24 13:33:02 +0300

    Migrate to Jest #19

--- test/test-api.js
+++ test/test-api.js
@@ -1,13 +1,11 @@
 const TvApi = require("../app");
-const {expect} = require("chai");
-require("mocha");
 
 describe("TvApi Should ", () => {
 
 	it("instantiate", () => {
 		const tvApi = new TvApi();
 
-		expect(tvApi).to.be.not.undefined;
+		expect(tvApi).not.toBeUndefined();
 	});
 
 });
